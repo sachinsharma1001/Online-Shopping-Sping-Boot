9bd1c8be3d365578728600102d9b1a3d09057610
sachinsharma1001 <sachin.sharma100197@gmail.com>
2021-04-21 17:45:07 +0530

    Add online shopping code

---
+++ src/main/java/com/ecom/dao/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.ecom.dao;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.ecom.entities.Product;
+
+@CrossOrigin
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+	Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+}
